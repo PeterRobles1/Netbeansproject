9b7c87e3453330cc451d6994565f8ebf216c7dba
Pedro <Pedro@DESKTOP-6K0JFP3>
2019-09-03 13:25:38 +0200

    test commit 2.0

---
+++ Redo/src/testclass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Pedro
+ */
+public class testclass {
+    /** Metodo para sumar
+     * 
+     * @param a Primera entrada
+     *
+     * @param b Segunda entrada
+     */
+    public void suma(int a,int b) {
+        int resultado = 0;
+        resultado= a+b;
+        System.out.println(resultado);
+    }
+}
